0390144c498ba16e4d8d9a84aa26f7e0051b1d66
REVANTH <121494570+LUCIFLASH122@users.noreply.github.com>
2026-01-09 23:37:00 +0530

    lab-2 exp-3

--- train.py
+++ train.py
@@ -4,48 +4,31 @@ import os
 import joblib
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.linear_model import Ridge
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = os.path.join(BASE_DIR, "dataset", "winequality-red.csv")
 
 OUTPUT_DIR = "outputs"
-
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 data = pd.read_csv(DATASET_PATH, sep=";")
 
-selected_features = ["alcohol", "sulphates", "volatile acidity"]
-X = data[selected_features]
+corr = data.corr()["quality"].abs()
+features = corr[corr > 0.1].index.tolist()
+features.remove("quality")
+
+X = data[features]
 y = data["quality"]
 
-
-
-MODEL_TYPE = "linear"
-USE_SCALER = True
-TEST_SIZE = 0.3
-
-
-
-
-
-if USE_SCALER:
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=TEST_SIZE, random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
-model = Ridge(alpha=1.0)
-
-
-
+model = LinearRegression()
 model.fit(X_train, y_train)
+
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
@@ -56,15 +39,8 @@ print("R2:", r2)
 
 joblib.dump(model, f"{OUTPUT_DIR}/model.pkl")
 
-with open(f"{OUTPUT_DIR}/results.json", "w") as f:
-    json.dump(
-        {
-            "model": MODEL_TYPE,
-            "scaler": USE_SCALER,
-            "test_size": TEST_SIZE,
-            "mse": mse,
-            "r2": r2
-        },
-        f,
-        indent=4
-    )
+json.dump(
+    {"experiment": "EXP-03", "mse": mse, "r2": r2},
+    open(f"{OUTPUT_DIR}/results.json", "w"),
+    indent=4
+)
