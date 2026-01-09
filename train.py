fbaa0004055fb3191b1331449a3e9d3bd5b204e5
REVANTH <121494570+LUCIFLASH122@users.noreply.github.com>
2026-01-09 23:47:46 +0530

    lab-2 exp-5

--- train.py
+++ train.py
@@ -4,8 +4,7 @@ import os
 import joblib
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -16,20 +15,14 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 data = pd.read_csv(DATASET_PATH, sep=";")
 
-corr = data.corr()["quality"].abs()
-features = corr[corr > 0.1].index.tolist()
-features.remove("quality")
-
-X = data[features]
+X = data.drop("quality", axis=1)
 y = data["quality"]
 
-X = StandardScaler().fit_transform(X)
-
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+model = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -43,7 +36,7 @@ print("R2:", r2)
 joblib.dump(model, f"{OUTPUT_DIR}/model.pkl")
 
 json.dump(
-    {"experiment": "EXP-04", "mse": mse, "r2": r2},
+    {"experiment": "EXP-05", "mse": mse, "r2": r2},
     open(f"{OUTPUT_DIR}/results.json", "w"),
     indent=4
 )
